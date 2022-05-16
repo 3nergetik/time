eb42ef1f00e418beea63b9b0c15b18bd70abf3c8
3nergetik <carbo_r86@mail.ru>
2022-05-16 20:57:07 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import withHOC from "./hoc/Wrapper";
+
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+const DateTimePretty = withHOC(DateTime);
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateTimePretty date={props.date}/>
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2021-07-31 12:26:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-03-03 11:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2011-02-03 13:13:13'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2022-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2020-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2021-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
